c4bc8de71fe4752c6537a07090057c38d3b742f8
Ryan-Biondo <biondoryan.dev@gmail.com>
2024-03-08 06:45:53 -0700

    Fixed Repairs, added messages

--- default/role.builder.js
+++ default/role.builder.js
@@ -1,32 +1,51 @@
-var roleBuilder = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.building = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-            creep.memory.building = true;
-            creep.say('🚧 build');
-        }
-
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
+const roleBuilder = {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+      creep.memory.building = false;
+      if (creep.fatigue == 0) creep.say("🔄 Harvest");
     }
+    if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+      creep.memory.building = true;
+      if (creep.fatigue == 0) creep.say("🚧 Build");
+    }
+
+    if (creep.memory.building) {
+      let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+      if (targets.length) {
+        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0], {
+            visualizePathStyle: { stroke: "#ffffff" },
+          });
+        }
+      } else {
+        // No construction sites, check for structures needing repair
+        let repairs = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => structure.hits < structure.hitsMax,
+        });
+        if (repairs.length) {
+          repairs.sort((a, b) => a.hits - b.hits); // Prioritize the most damaged
+
+          if (creep.memory.lastRepairTarget !== repairs[0].id) {
+            creep.say("🔨 Repair");
+            // Update lastRepairTarget in memory
+            creep.memory.lastRepairTarget = repairs[0].id;
+          }
+
+          if (creep.repair(repairs[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(repairs[0], {
+              visualizePathStyle: { stroke: "#ffaa00" },
+            });
+          }
+        }
+      }
+    } else {
+      let sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+      }
+    }
+  },
 };
 
 module.exports = roleBuilder;
--- default/role.harvester.js
+++ default/role.harvester.js
@@ -1,30 +1,37 @@
-var roleHarvester = {
+const roleHarvester = {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    if (creep.store.getFreeCapacity() > 0) {
+      let sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        if (creep.fatigue == 0) creep.say("🔄 Harvest");
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+      }
+    } else {
+      let targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (
+            (structure.structureType == STRUCTURE_TOWER ||
+              structure.structureType == STRUCTURE_EXTENSION ||
+              structure.structureType == STRUCTURE_SPAWN) &&
+            structure.store &&
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+          );
+        },
+      });
+
+      if (targets.length > 0) {
+        if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          if (creep.fatigue == 0) creep.say("🚚 Deliver");
+
+          creep.moveTo(targets[0], {
+            visualizePathStyle: { stroke: "#ffffff" },
+          });
         }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_ROAD ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
+      }
     }
+  },
 };
 
 module.exports = roleHarvester;
