a1a9cf26c399208b1da636d320da391a722964c5
Ryan-Biondo <biondoryan.dev@gmail.com>
2024-03-11 17:12:09 -0600

    Reduce towerControl to build up

--- default/towerControl.js
+++ default/towerControl.js
@@ -1,11 +1,11 @@
 const towerControl = {
   run: function (tower) {
     // Attack hostiles
-    let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestHostile) {
-      tower.attack(closestHostile);
-      return;
-    }
+    // let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    // if (closestHostile) {
+    //   tower.attack(closestHostile);
+    //   return;
+    // }
 
     // Heal creeps
     let closestDamagedCreep = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
@@ -18,15 +18,15 @@ const towerControl = {
     }
 
     // Repair structures
-    let closestDamagedStructure = tower.pos.findClosestByRange(
-      FIND_STRUCTURES,
-      {
-        filter: (structure) => structure.hits < structure.hitsMax,
-      }
-    );
-    if (closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-    }
+    // let closestDamagedStructure = tower.pos.findClosestByRange(
+    //   FIND_STRUCTURES,
+    //   {
+    //     filter: (structure) => structure.hits < structure.hitsMax,
+    //   }
+    // );
+    // if (closestDamagedStructure) {
+    //   tower.repair(closestDamagedStructure);
+    // }
   },
 };
 
