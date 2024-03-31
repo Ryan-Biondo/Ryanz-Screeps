271654be888b50866dcc78531ef65c48b3389f16
Ryan-Biondo <biondoryan.dev@gmail.com>
2024-03-31 08:45:47 -0600

    Increase harvesters

--- default/main.js
+++ default/main.js
@@ -105,7 +105,7 @@ module.exports.loop = function () {
       }
     );
   }
-  if (harvesters.length < 8) {
+  if (harvesters.length < 9) {
     let newName = "HarvesterBigger" + Game.time;
     console.log("Harvesters: " + harvesters.length);
     console.log("Spawning new harvester: " + newName);
